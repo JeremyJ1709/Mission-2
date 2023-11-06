97f917eb0c2569011a06531c88a58ba86fef3b15
Jeremy Jebaseelan <jer.jebaseelan@gmail.com>
2023-11-06 17:32:35 +1300

    first commit

---
+++ mission2.js
@@ -0,0 +1,36 @@
+function hideContent1() {
+  let showHide = document.getElementById("chicago");
+  let button = document.getElementById("chicagoButton");
+  if (showHide.style.display === "none") {
+    showHide.style.display = "block";
+  } else {
+    showHide.style.display = "none";
+  }
+}
+function hideContent2() {
+  let showHide = document.getElementById("dallas");
+  let button = document.getElementById("dallasButton");
+  if (showHide.style.display === "none") {
+    showHide.style.display = "block";
+  } else {
+    showHide.style.display = "none";
+  }
+}
+function hideContent3() {
+  let showHide = document.getElementById("colorado");
+  let button = document.getElementById("cosButton");
+  if (showHide.style.display === "none") {
+    showHide.style.display = "block";
+  } else {
+    showHide.style.display = "none";
+  }
+}
+function hideContent4() {
+  let showHide = document.getElementById("LA");
+  let button = document.getElementById("laButton");
+  if (showHide.style.display === "none") {
+    showHide.style.display = "block";
+  } else {
+    showHide.style.display = "none";
+  }
+}
